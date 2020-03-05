a264ab6d5986cd11bb16df76f1d08445258a9775
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-03-05 14:11:42 +0200

    Refactor burger menu display

--- templates/scripts/create-political-party.js
+++ templates/scripts/create-political-party.js
@@ -1,6 +1,7 @@
 const burgerMenu = document.querySelector('#burger');
 const menuItems = document.querySelector('.menu-items');
 const viewParties = document.querySelector('#view-parties');
+const createParty = document.querySelector('#create-party');
 const navViewParties = document.querySelector('#nav-view-parties');
 const navCreateParty = document.querySelector('#nav-create-party')
 
@@ -8,6 +9,10 @@ menuItems.classList.add('hide');
 
 burgerMenu.addEventListener('click', () => {
     menuItems.classList.toggle('hide');
+});
+
+createParty.addEventListener('click', () => {
+    window.location.href='../html/create-political-party.html';
 });
 
 viewParties.addEventListener('click', () => {
