20d3e4e20ddace7ac83f7d08709286134afe76f7
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-03-07 13:17:46 +0200

    Draft voting history page

---
+++ templates/scripts/voting-history.js
@@ -0,0 +1,20 @@
+const burgerMenu = document.querySelector('#burger');
+const menuItems = document.querySelector('.menu-items');
+const viewParties = document.querySelector('#view-parties');
+const navViewParties = document.querySelector('#nav-view-parties');
+const history = document.querySelector('#history');
+const navHistory = document.querySelector('#nav-history');
+
+menuItems.classList.add('hide');
+
+burgerMenu.addEventListener('click', () => {
+    menuItems.classList.toggle('hide');
+});
+
+viewParties.addEventListener('click', () => {
+    window.location.href='../html/user-view-parties.html';
+});
+
+navViewParties.addEventListener('click', () => {
+    window.location.href='../html/user-view-parties.html';
+});
