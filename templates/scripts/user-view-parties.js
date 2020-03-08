46aaca1d99a4243a715f92a2419756e3e5e87440
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-03-08 22:14:46 +0200

    Align party names and headquarters

--- templates/scripts/user-view-parties.js
+++ templates/scripts/user-view-parties.js
@@ -1,7 +1,11 @@
 const burgerMenu = document.querySelector('#burger');
 const menuItems = document.querySelector('.menu-items');
 const viewParties = document.querySelector('#view-parties');
-const navViewParties = document.querySelector('#nav-view-parties')
+const navViewParties = document.querySelector('#nav-view-parties');
+const viewCandidates = document.querySelector('#view-candidates');
+const navViewCandidates = document.querySelector('#nav-view-candidates');
+const history = document.querySelector('#history');
+const navHistory = document.querySelector('#nav-history');
 
 menuItems.classList.add('hide');
 
@@ -15,4 +19,20 @@ viewParties.addEventListener('click', () => {
 
 navViewParties.addEventListener('click', () => {
     window.location.href='../html/user-view-parties.html';
+});
+
+viewCandidates.addEventListener('click', () => {
+    window.location.href='../html/view-politicians.html';
+});
+
+navViewCandidates.addEventListener('click', () => {
+    window.location.href='../html/view-politicians.html';
+});
+
+history.addEventListener('click', () => {
+    window.location.href='../html/voting-history.html';
+});
+
+navHistory.addEventListener('click', () => {
+    window.location.href='../html/voting-history.html';
 });
