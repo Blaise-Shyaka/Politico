8666e45bebc924564e4ecfdc08356b80e9a71cb9
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2020-10-02 09:18:31 +0200

    remove console statements

--- server/data/config.js
+++ server/data/config.js
@@ -4,18 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 let connectionString = '';
-console.log(`hey, testing ${connectionString} end`)
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
   connectionString = process.env.DATABASE_URL;
 } else if (process.env.NODE_ENV === 'dev') {
-  console.log('inside database')
   connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DEV}`;
 } else if (process.env.NODE_ENV === 'test') {
   connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_TEST}`;
 }
-
-console.log(`end ${connectionString}end`);
 
 const pool = new Pool({ connectionString });
 
